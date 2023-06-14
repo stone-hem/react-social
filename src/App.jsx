5a96dbc8b939abe4afeb0d4d7288a0d28f53fbbf
Hemstone <hemstone456@gmail.com>
2023-06-14 12:58:18 +0300

    react redux js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Profile from "./pages/profile"
 
 function App() {
 
   return (
   <div className="app">
-
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/profile/:userId" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { authSlice } from './state/index.js'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authSlice)
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE]
+      }
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
