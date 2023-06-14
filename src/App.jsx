4ae48e03b971f3fcdad4a03a5c574dae317c1fe1
Hemstone <hemstone456@gmail.com>
2023-06-14 13:05:55 +0300

    apply theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Login from "./pages/login"
 import Profile from "./pages/profile"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { themeSettings } from "./theme"
 
 function App() {
-
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-  <div className="app">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/profile/:userId" element={<Profile/>}/>
-    </Routes>
-    </BrowserRouter>
-  </div>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
